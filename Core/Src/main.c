02606fef641fd9edc1605697939c64deb51a3ae5
Marc Goodner <mgoodner@microsoft.com>
2023-03-04 20:04:42 -0800

    Add readme

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -88,6 +88,61 @@ static void MX_ICACHE_Init(void);
   * @brief  The application entry point.
   * @retval int
   */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* Configure the System Power */
+  SystemPower_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_ADF1_Init();
+  MX_I2C1_Init();
+  MX_I2C2_Init();
+  MX_OCTOSPI1_Init();
+  MX_OCTOSPI2_Init();
+  MX_SPI2_Init();
+  MX_UART4_Init();
+  MX_USART1_UART_Init();
+  MX_UCPD1_Init();
+  MX_USB_OTG_FS_PCD_Init();
+  MX_ICACHE_Init();
+  /* USER CODE BEGIN 2 */
+
+  /* USER CODE END 2 */
+
+  MX_ThreadX_Init();
+
+  /* We should never get here as control is now taken by the scheduler */
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
 
 /**
   * @brief System Clock Configuration
